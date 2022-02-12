726ba9ce259886e1e5ffb25200f97be7d0bedbce
Jesús Enrique Cabeza Acero <jesusenriquecabezaacero@gmail.com>
2022-02-12 17:14:12 -0500

    Creando las rutas de módulo de Autorización AuthRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 import { NofoundComponent } from './nofound/nofound.component';
 import { PagesRoutingModule } from './pages/pages-routing.module';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: '**', component: NofoundComponent },
 ]
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [ RouterModule]
 })
