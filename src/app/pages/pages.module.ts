58ed5ff1f0add0319d9333a93e291f3affbe23f9
Jesús Enrique Cabeza Acero <jesusenriquecabezaacero@gmail.com>
2022-02-28 09:59:16 -0500

    Cambiando de manera manual el valor de la barra de progreso

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GraficaComponent } from './grafica/grafica.component';
 import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
+
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
 
@@ -19,7 +22,8 @@ import { AppRoutingModule } from '../app-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   exports: [
     DashboardComponent,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,14 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
+  progress: number = 50;
   constructor() { }
 
-  ngOnInit(): void {
+  get getProgress(): string {
+    return `${this.progress}%`;
   }
 
+  changeValue(value: number) {
+    if (this.progress === 0 && value < 0) {
+      this.progress = 0;
+      return;
+    }
+    if (this.progress === 100 && value > 0) {
+      this.progress = 100;
+      return;
+    }
+    this.progress += value;
+  }
 }
